2737a211478a2bb75d29a3455bf0039b1583f3bd
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-25 12:16:50 -0500

    I remove any hard-coded references to RPC url.

--- web/src/store/createWagmiConfig.ts
+++ web/src/store/createWagmiConfig.ts
@@ -1,5 +1,5 @@
 import { createConfig, http } from 'wagmi';
-import { base, baseSepolia } from 'wagmi/chains';
+import { baseSepolia } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
 
 export function createWagmiConfig(rpcUrl: string, projectId?: string) {
@@ -9,7 +9,6 @@ export function createWagmiConfig(rpcUrl: string, projectId?: string) {
   }
 
   // Temporary hack, until we configure a FE page in OnchainKit to copy just the API key
-  const baseUrl = rpcUrl.replace(/\/v1\/(.+?)\//, '/v1/base/');
   const baseSepoliaUrl = rpcUrl.replace(/\/v1\/(.+?)\//, '/v1/base-sepolia/');
 
   return createConfig({
@@ -17,12 +16,12 @@ export function createWagmiConfig(rpcUrl: string, projectId?: string) {
     connectors: [
       coinbaseWallet({
         appName: 'buildonchainapps',
+        preference: 'smartWalletOnly'
       }),
     ],
     ssr: true,
     transports: {
       [baseSepolia.id]: http(baseSepoliaUrl),
-      [base.id]: http(baseUrl),
     },
   });
 }
